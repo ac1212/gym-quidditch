fbad1fe8c45e02ec6195fa65bde31ad64ef6e61d
achatterjee36 <achatterjee36@gatech.edu>
2019-05-09 13:01:17 -0400

    added gif

--- demo.py
+++ demo.py
@@ -1,12 +1,19 @@
 import gym
 
+GENERATE_FRAMES = True
+if GENERATE_FRAMES:
+    import scipy.misc
+render_mode = 'rgb_array' if GENERATE_FRAMES else 'human'
+
 env = gym.make('gym_quidditch:quidditch-snitch-v0')
 env.reset()
 
 for i_episode in range(5):
     observation = env.reset()
     for t in range(1000):
-        env.render()
+        rgb = env.render(mode=render_mode)
+        if GENERATE_FRAMES:
+            scipy.misc.imsave('frames/frames_e{:04d}-t{:04d}.png'.format(i_episode,t),rgb)
         #print(observation)
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
